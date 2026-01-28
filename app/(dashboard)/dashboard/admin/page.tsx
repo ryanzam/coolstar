281a0ade6a3510b2566bdc4c8493d81644f74dfb
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-01-28 10:05:16 +0545

    fix ui for admin booking list

--- app/(dashboard)/dashboard/admin/page.tsx
+++ app/(dashboard)/dashboard/admin/page.tsx
@@ -10,6 +10,22 @@ const DashboardAdminPage = async ({ user }: DashboardAdminPageProps) => {
 
     const { data: bookings } = await getAllBookings();
 
+    const newBookings = bookings?.filter(booking => booking.status === 'PENDING') || [];
+    const oldBookings = bookings?.filter(booking => booking.status !== 'PENDING') || [];
+
+    const getNewBookings = () => {
+        if (newBookings.length === 0) {
+            return <div className='bg-white border border-gray-400 rounded-md py-3 px-5'>
+                You don't have any pending service.
+            </div>
+        }
+        return (
+            newBookings.map((booking: any) => (
+                <BookingCollapsible key={booking.id} booking={booking} />
+            ))
+        )
+    }
+
     return (
         <main className="pt-10 min-h-screen frost-bg">
             <section className="py-16">
@@ -18,10 +34,18 @@ const DashboardAdminPage = async ({ user }: DashboardAdminPageProps) => {
                     <BookingStats bookings={bookings} />
 
                     {bookings && bookings?.length > 0 ? (
-                        <div className='grid grid-cols-1 gap-2'>
-                            {bookings.map((booking: any) => (
-                                <BookingCollapsible key={booking.id} booking={booking} />
-                            ))}
+                        <div className='flex gap-8 flex-col'>
+                            <div className='grid grid-cols-1 gap-2'>
+                                <p className="font-semibold pb-2">Pending booking (s)</p>
+                                {getNewBookings()}
+                            </div>
+
+                            <div>
+                                <p className="font-semibold pb-2">Completed</p>
+                                {oldBookings.map((booking: any) => (
+                                    <BookingCollapsible key={booking.id} booking={booking} />
+                                ))}
+                            </div>
                         </div>
                     ) : (
                         <p>No pending bookings.</p>
@@ -29,7 +53,7 @@ const DashboardAdminPage = async ({ user }: DashboardAdminPageProps) => {
 
                 </div>
             </section>
-        </main>
+        </main >
     )
 }
 
